ccda8c71496223a8a6bc86f4cfcf52a5faf2a372
yambikov <yambikov@yandex.ru>
2024-01-24 15:16:07 +0300

    07 Routes and controllers updated

--- routes/cards.js
+++ routes/cards.js
@@ -5,8 +5,8 @@ const {
   createCard,
   getCards,
   deleteCard,
-  addCardLike,
-  removeCardLike,
+  // addCardLike,
+  // removeCardLike,
 } = require('../controllers/cards');
 
 const {
@@ -17,7 +17,31 @@ const {
 cardsRouter.post('/', validateCreateCard, createCard); // полный путь /cards/
 cardsRouter.get('/', getCards); // полный путь /cards/
 cardsRouter.delete('/:cardId', validateCardId, deleteCard); // полный путь /cards/:cardId
-cardsRouter.put('/:cardId/likes', validateCardId, addCardLike); // полный путь /cards/:cardId
-cardsRouter.delete('/:cardId/likes', validateCardId, removeCardLike);
+// cardsRouter.put('/:cardId/likes', validateCardId, addCardLike); // полный путь /cards/:cardId
+// cardsRouter.delete('/:cardId/likes', validateCardId, removeCardLike);
 
 module.exports = cardsRouter;
+
+// // routes/cards.js
+
+// const cardsRouter = require('express').Router(); // создаем роуты карточек
+// const {
+//   createCard,
+//   getCards,
+//   deleteCard,
+//   addCardLike,
+//   removeCardLike,
+// } = require('../controllers/cards');
+
+// const {
+//   validateCreateCard,
+//   validateCardId,
+// } = require('../middlewares/validation');
+
+// cardsRouter.post('/', validateCreateCard, createCard); // полный путь /cards/
+// cardsRouter.get('/', getCards); // полный путь /cards/
+// cardsRouter.delete('/:cardId', validateCardId, deleteCard); // полный путь /cards/:cardId
+// cardsRouter.put('/:cardId/likes', validateCardId, addCardLike); // полный путь /cards/:cardId
+// cardsRouter.delete('/:cardId/likes', validateCardId, removeCardLike);
+
+// module.exports = cardsRouter;
--- routes/users.js
+++ routes/users.js
@@ -2,24 +2,52 @@
 
 const userRouter = require('express').Router(); // создаем роуты юзера
 const {
-  getUsers,
-  getUserById,
+  // getUsers,
+  // getUserById,
   updateUser,
-  updateAvatar,
+  // updateAvatar,
   getCurrentUser,
 } = require('../controllers/users');
 
 const {
   validateUpdateUser,
-  validateUpdateAvatar,
-  validateUserId,
+  // validateUpdateAvatar,
+  // validateUserId,
 } = require('../middlewares/validation');
 
-userRouter.get('/', getUsers); // полный путь /users/ так как в app.use('/users', userRouter);
+// userRouter.get('/', getUsers); // полный путь /users/ так как в app.use('/users', userRouter);
 userRouter.get('/me', getCurrentUser); // полный путь /users/me
-userRouter.get('/:userId', validateUserId, getUserById); // полный путь /users/:userId'
+// userRouter.get('/:userId', validateUserId, getUserById); // полный путь /users/:userId'
 
 userRouter.patch('/me', validateUpdateUser, updateUser); // полный путь /users/me
-userRouter.patch('/me/avatar', validateUpdateAvatar, updateAvatar); // полный путь /users/me/avatar
+// userRouter.patch('/me/avatar', validateUpdateAvatar, updateAvatar);
+// полный путь /users/me/avatar
 
 module.exports = userRouter;
+
+// // routes/users.js
+
+// const userRouter = require('express').Router(); // создаем роуты юзера
+// const {
+//   getUsers,
+//   getUserById,
+//   updateUser,
+//   updateAvatar,
+//   getCurrentUser,
+// } = require('../controllers/users');
+
+// const {
+//   validateUpdateUser,
+//   validateUpdateAvatar,
+//   validateUserId,
+// } = require('../middlewares/validation');
+
+// userRouter.get('/', getUsers); // полный путь /users/ так как в app.use('/users', userRouter);
+// userRouter.get('/me', getCurrentUser); // полный путь /users/me
+// userRouter.get('/:userId', validateUserId, getUserById); // полный путь /users/:userId'
+
+// userRouter.patch('/me', validateUpdateUser, updateUser); // полный путь /users/me
+// userRouter.patch('/me/avatar', validateUpdateAvatar, updateAvatar); -
+// полный путь /users/me/avatar
+
+// module.exports = userRouter;
