7cc345070cf3b689d48ccf92f6b6a0446b151d03
yambikov <yambikov@yandex.ru>
2024-01-24 14:33:38 +0300

    06 cardShema updated
    but need to rename to "movie"

--- models/card.js
+++ models/card.js
@@ -4,36 +4,106 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 const cardSchema = new mongoose.Schema({
-  name: {
-    type: String, // тип данных - строка
-    required: true, // обязательное поле
-    minlength: 2, // минимальная длина строки
-    maxlength: 30, // максимальная длина строки
+  country: {
+    type: String,
+    required: true,
   },
-  link: {
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
     type: String,
     required: true,
     validate: {
-      validator: (string) => {
-        validator.isURL(string);
-      },
+      validator: (string) => validator.isURL(string),
+      message: 'Неправильный формат ссылки',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (string) => validator.isURL(string),
+      message: 'Неправильный формат ссылки',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (string) => validator.isURL(string),
       message: 'Неправильный формат ссылки',
     },
   },
   owner: {
-    type: mongoose.Schema.Types.ObjectId, // связь карточки с моделью пользователя
-    ref: 'user', // ссылка на модель автора карточки
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
     required: true,
   },
-  likes: [{
-    type: mongoose.Schema.Types.ObjectId, // связь карточки с моделью пользователя
-    ref: 'user',
-    default: [],
-  }],
-  createdAt: {
-    type: Date,
-    default: Date.now,
+  movieId: {
+    type: Number,
+    required: true,
   },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+
 });
 
 module.exports = mongoose.model('card', cardSchema);
+
+// const mongoose = require('mongoose');
+// const validator = require('validator');
+
+// const cardSchema = new mongoose.Schema({
+//   name: {
+//     type: String, // тип данных - строка
+//     required: true, // обязательное поле
+//     minlength: 2, // минимальная длина строки
+//     maxlength: 30, // максимальная длина строки
+//   },
+//   link: {
+//     type: String,
+//     required: true,
+//     validate: {
+//       validator: (string) => {
+//         validator.isURL(string);
+//       },
+//       message: 'Неправильный формат ссылки',
+//     },
+//   },
+//   owner: {
+//     type: mongoose.Schema.Types.ObjectId, // связь карточки с моделью пользователя
+//     ref: 'user', // ссылка на модель автора карточки
+//     required: true,
+//   },
+//   likes: [{
+//     type: mongoose.Schema.Types.ObjectId, // связь карточки с моделью пользователя
+//     ref: 'user',
+//     default: [],
+//   }],
+//   createdAt: {
+//     type: Date,
+//     default: Date.now,
+//   },
+// });
+
+// module.exports = mongoose.model('card', cardSchema);
